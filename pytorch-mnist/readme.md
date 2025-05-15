5aa1445a29cb9d24b2407a0e5a8b4e7860c613f2
kbcv <dreamkyh@163.com>
2025-05-16 00:54:05 +0800

    [add]pytorch手写数字识别实现

---
+++ pytorch-mnist/readme.md
@@ -0,0 +1,4 @@
+
+# 环境安装
+pip install opencv
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
---
+++ pytorch-mnist/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import torch
+from torchvision import transforms
+
+from PIL import Image, ImageOps
+from model import NeuralNetwork
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'using {device}')
+model = NeuralNetwork().to(device)
+path = 'mnist.pth'
+model.load_state_dict(torch.load(path,weights_only=True))
+
+print(model)
+
+
+def test(path):
+    print(f'test {path}...')
+    image = Image.open(path).convert('RGB').resize((28, 28))
+    image = ImageOps.invert(image)
+
+    trans = transforms.Compose([
+        transforms.Grayscale(1),
+        transforms.ToTensor()
+    ])
+    image_tensor = trans(image).unsqueeze(0).to(device)
+    model.eval()
+    with torch.no_grad():
+        output = model(image_tensor)
+        probs = torch.nn.functional.softmax(output[0], 0)
+    predict = torch.argmax(probs).item()
+    return predict, probs[predict], probs
+
+
+def main():
+    for i in range(10):
+        predict, prob, probs = test(f'input/test-{i}.png')
+        print(f'真实值： {i}, 预测值： {predict:}, 分数：{prob:.2f}')
+
+
+if __name__ == '__main__':
+    main()
